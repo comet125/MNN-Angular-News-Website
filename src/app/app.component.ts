d4cdd6b73df1da0e2eeab3edd2b0a6d500868993
Mateo Marjanovic <mmateo928@gmail.com>
2025-01-02 00:42:03 +0100

    fixing deployment issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavBarComponent} from './nav-bar/nav-bar.component';
+/*import {NavBarComponent} from './nav-bar/nav-bar.component';
 import {FooterComponent} from './footer/footer.component';
 import {LoginPageComponent} from './login-page/login-page.component';
-import {SignupPageComponent} from './signup-page/signup-page.component';
+import {SignupPageComponent} from './signup-page/signup-page.component';*/
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, FooterComponent, LoginPageComponent, SignupPageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
