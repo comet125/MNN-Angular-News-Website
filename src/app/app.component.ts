ae1bcd0a96e8a2a320633aa7cb54ee29e0698146
Mateo Marjanovic <mmateo928@gmail.com>
2025-01-01 23:00:58 +0100

    git init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {FooterComponent} from './footer/footer.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {SignupPageComponent} from './signup-page/signup-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, LoginPageComponent, SignupPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginPageComponent }, // Default route
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'home', component: HomePageComponent },
+];
+
