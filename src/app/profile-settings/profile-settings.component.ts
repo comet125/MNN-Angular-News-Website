345889b23899a74517a02ac94ba4848c3e70efea
Mateo Marjanovic <mmateo928@gmail.com>
2025-01-05 20:27:11 +0100

    user settings functionality

--- src/app/profile-settings/profile-settings.component.ts
+++ src/app/profile-settings/profile-settings.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {Router, RouterLink} from '@angular/router';
 import { OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-profile-settings',
   imports: [
     FormsModule,
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   templateUrl: './profile-settings.component.html',
   styleUrl: './profile-settings.component.css'
@@ -16,51 +19,73 @@ import { OnInit } from '@angular/core';
 
 
 export class ProfileSettingsComponent implements OnInit {
+  permanentFirstName: string | null = '';
+  permanentSurname: string | null = '';
+
   firstName: string = '';
   surname: string = '';
   oldPassword: string = '';
   newPassword: string = '';
   confirmPassword: string = '';
+  username: string | null = null;
 
+  constructor(private router: Router, private http: HttpClient) {}
 
-  constructor(private router: Router) { }
+  ngOnInit(): void {
+    // Get username from localStorage
+    this.username = localStorage.getItem('username');
+    this.permanentFirstName = localStorage.getItem('first_name');
+    this.permanentSurname = localStorage.getItem('surname');
 
+    this.firstName = localStorage.getItem('first_name') || '';
+    this.surname = localStorage.getItem('surname') || '';
 
+    if (!this.username) {
+      alert('Session timed out. Please log out and log back in again');
+    }
+  }
 
-
-  // Function to check if the new passwords match
+  // Check if new passwords match
   formValid(): boolean {
     return this.newPassword === this.confirmPassword;
   }
 
-
-  // Function to handle form submission
-  firstname: string | null | undefined;
-  lastname: string | null | undefined;
-  username: string | null | undefined;
-
-  ngOnInit() {
-    this.firstname = localStorage.getItem('first_name');
-    this.lastname = localStorage.getItem('surname');
-    this.username = localStorage.getItem('username');
-  }
-
+  // Handle form submission
   onSubmit(): void {
     if (!this.formValid()) {
-      // If passwords don't match, show an alert
       alert('New passwords do not match. Please try again.');
       return;
     }
 
-    // Process the form submission here
-    // For now, just print the values to console
-    console.log('Profile updated:', {
-      firstName: this.firstName,
+    if (!this.firstName || !this.surname || !this.oldPassword || !this.newPassword) {
+      alert('All fields are required.');
+      return;
+    }
+
+    const updatedProfile = {
+      username: this.username,
+      first_name: this.firstName,
       surname: this.surname,
       oldPassword: this.oldPassword,
       newPassword: this.newPassword,
-    });
+    };
 
-    // Here, you can add any service to update user profile data, etc.
+    this.http.post<any>('http://localhost/database/editprofile.php', updatedProfile).subscribe(
+      response => {
+        if (response.status === 'success') {
+          alert('Profile updated successfully.');
+          localStorage.setItem('first_name', this.firstName);
+          localStorage.setItem('surname', this.surname);
+        } else {
+          alert(response.message);
+        }
+      },
+      error => {
+        alert('An error occurred while updating the profile. Please check if the old password is correct.');
+        console.error(error);
+      }
+    );
   }
 }
+
+
